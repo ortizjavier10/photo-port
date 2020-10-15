6dd906502ee1674dfc891b2c50ce961f9414a221
JAVI <ortiz.javier10@gmail.com>
2020-10-15 18:09:48 -0400

    gallery

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { capitalizeFirstLetter } from '../../utils/helpers';
 
 function Nav(props) {
+    useEffect(() => {
+        document.title = capitalizeFirstLetter(currentCategory.name);
+    }, [currentCategory]);
   const {
     categories = [],
     setCurrentCategory,
